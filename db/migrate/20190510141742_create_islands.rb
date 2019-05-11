3decbd995d27fe6fc9bca9b321db28cc78cdd9f5
Kumi Takenaka <kumireliy@gmail.com>
2019-05-11 13:06:52 +0900

    responsive design

---
+++ db/migrate/20190510141742_create_islands.rb
@@ -0,0 +1,10 @@
+class CreateIslands < ActiveRecord::Migration[5.2]
+  def change
+    create_table :islands do |t|
+      t.string :name
+      t.text :about
+
+      t.timestamps
+    end
+  end
+end
