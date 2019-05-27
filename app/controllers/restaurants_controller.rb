a38faefab43ac2874d6425c045fd5941c2cbb1a4
Kumi Takenaka <kumireliy@gmail.com>
2019-05-27 13:22:43 +0900

    created restaurants index page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,20 @@
 class RestaurantsController < ApplicationController
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index], raise: false
+
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def create
+    @restaurant = Restaurant.new
   end
 
   def destroy
+    @restaurant = Restaurant.find(params[:id])
   end
 end
